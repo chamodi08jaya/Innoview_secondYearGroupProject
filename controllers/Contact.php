3fd8d345d2af6cb3cd01315d9fe4839b2533c780
Chamodi Jayamini <36620277+chamodi08jaya@users.noreply.github.com>
2019-10-17 15:18:59 +0530

    controllers

--- controllers/Contact.php
+++ controllers/Contact.php
@@ -3,9 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contact extends CI_Controller {
 
-	
-	public function index()
-	{
-		$this->load->view('contact.php');
+	public function index{
+		$this->load->view('home');
 	}
+	public function about()
+	{
+		$this->load->view('about');
+	}
+	public function class_schedule()
+	{
+		$this->load->view('class_schedule');
+	}
+	public function contact()
+	{
+		$this->load->view('contact');
+	}
+	public function login()
+	{
+		$this->load->view('login');
+	}
+	public function registration()
+	{
+		$this->load->view('registration');
+	}
+	
 }
--- controllers/Login.php
+++ controllers/Login.php
@@ -4,8 +4,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
 
 	
-	public function index()
+	public function index(){
+		$this->load->view('home');
+	}
+	public function about()
 	{
-		$this->load->view('login.php');
+		$this->load->view('about');
+	}
+	public function class_schedule()
+	{
+		$this->load->view('class_schedule');
+	}
+	public function contact()
+	{
+		$this->load->view('contact');
+	}
+	public function login()
+	{
+		$this->load->view('login');
+	}
+	public function registration()
+	{
+		$this->load->view('registration');
 	}
 }

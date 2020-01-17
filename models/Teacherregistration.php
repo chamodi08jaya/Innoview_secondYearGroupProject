ffeff5f35991cb2004a0762ab531b9728489c22d
Banuka0720 <36620038+Banuka0720@users.noreply.github.com>
2020-01-17 16:07:18 +0530

    Latest Models 17 Jan 2020

--- models/Teacherregistration.php
+++ models/Teacherregistration.php
@@ -4,7 +4,7 @@ class Teacherregistration extends CI_Model{
 
 public function registeruser(){
 $data1 = array(
-'user_id'=> $this->input->post('emp_no'),
+'user_id'=> $this->input->post('teacher_id'),
 'user_type'=> $this->input->post('user_type'),
 'user_name'=> $this->input->post('user_name'),
 'password'=> $this->input->post('password'),
@@ -12,25 +12,25 @@ $data1 = array(
 $this->db->insert('users',$data1);
 
 $data2 = array(
-'teacherid_fk'=> $this->input->post('teacherid'),
-'courseid_fk'=> $this->input->post('courseid'),
+'teacherid_fk'=> $this->input->post('student_id'),
+'courseid_fk'=> $this->input->post('course_id'),
 'hall_no'=> $this->input->post('hall_no'),
-'numofstudents'=> $this->input->post('numof_students'),
+'numofstudents'=> $this->input->post('numofstu'),
 );
 $this->db->insert('teacher_teach',$data2);
 
 $data3 = array(
-'course_id'=> $this->input->post('courseid'),
+'course_id'=> $this->input->post('course_id'),
 'subject'=> $this->input->post('subject'),
 'grade'=> $this->input->post('grade'),
-'teacher_fk'=> $this->input->post('teacher'),
-);
-$this->db->insert('teacher_teach',$data3);
+'teacher_fk'=> $this->input->post('teacher_id'),
+);			
+$this->db->insert('course',$data3);
 
 $data = array(
 'emp_no'=> $this->input->post('teacher_id'),
 'name'=> $this->input->post('name'),
-'gender'=> $this->input->post('gender'),
+//'gender'=> $this->input->post('gender'),
 'contact_no'=> $this->input->post('contact_number'),
 'email'=> $this->input->post('email'),
 );
